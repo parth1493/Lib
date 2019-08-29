f72d49803d76eb9907ba2fbc6ffa3f7800341168
Parth <parth1493@gmail.com>
2019-08-28 21:24:09 -0400

    added phone class

--- src/main/java/Phone.java
+++ src/main/java/Phone.java
@@ -1,2 +1,37 @@
 public class Phone {
+
+    private String contactName;
+    private String contactNumber;
+    private String contactType;
+
+    public Phone(String contactName, String contactNumber, String contactType) {
+        this.contactName = contactName;
+        this.contactNumber = contactNumber;
+        this.contactType = contactType;
+    }
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public String getContactType() {
+        return contactType;
+    }
+
+    public void setContactType(String contactType) {
+        this.contactType = contactType;
+    }
 }
+
